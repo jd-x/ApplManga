c6e01c4ad111bb4fa2e1501cdf4995dbaba8e76b
Jade Lausa <jademlausa@gmail.com>
2017-03-22 02:28:57 +0800

    Initial commit

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace ApplMango {
+namespace ApplManga {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            // Code snippet adapted from: http://www.wpf-tutorial.com/wpf-application/handling-exceptions/
+            // TODO: Exception handling
+            MessageBox.Show("An unhandled error just occured: " + e.Exception.Message, "Oops", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,26 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 
-namespace ApplMango {
+namespace ApplManga {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : MetroWindow {
         public MainWindow() {
             InitializeComponent();
+            //dataGrid.AutoGenerateColumns = true;
+
+            //dataGrid.ItemsSource = Environment.GetEnvironmentVariables()
+            //    .Cast<DictionaryEntry>().ToList();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+
         }
     }
 }
